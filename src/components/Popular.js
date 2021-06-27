1acf0a033863a67e87fce280bdece8d8a2a46aee
narzikulovv <sardornarzikulov2002@gmail.com>
2021-06-27 20:48:39 +0500

    rasmlarni to'g'irladm

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -15,29 +15,71 @@ const Popular = () => {
             <div className="container popular_con">
                 <div className="row popular_row">
                     <div className="col-12 popular_col12">
-                        <h1 className="popular_h1">Popular product</h1>
+                        <div className="container">
+                            <h1 className="popular_h1">Popular product</h1>
+                        </div>
                     </div>
                     <div className="col-12 popular_col_slic">
                         <Slider {...settings}>
-                            <div className="col-6">
-                                <div className="card">
-                                    <img src="" alt=""/>
+                            <div className="col-12 slic_col12">
+                                <div className="card slic_card">
+                                    <img src="../images/prodcut.png" width="100%" alt=""/>
+                                    <div className="write">
+                                        <h1>Clyde Core Foil</h1>
+                                        <h2>Women's Sneakers</h2>
+                                        <button type="button" className="btn btn-light">SHOP</button>
+                                    </div>
                                 </div>
                             </div>
-                            <div className="col-6">
-                                <h3>2</h3>
+                            <div className="col-12 slic_col12">
+                                <div className="card slic_card">
+                                    <img src="../images/product2.png" width="100%" alt=""/>
+                                    <div className="write">
+                                        <h1>RS-Fast INTL </h1>
+                                        <h2>men's Sneakers</h2>
+                                        <button type="button" className="btn btn-light">SHOP</button>
+                                    </div>
+                                </div>
                             </div>
-                            <div>
-                                <h3>3</h3>
+                            <div className="col-12 slic_col12">
+                                <div className="card slic_card">
+                                    <img src="../images/prodcut.png" width="100%" alt=""/>
+                                    <div className="write">
+                                        <h1>Clyde Core Foil</h1>
+                                        <h2>Women's Sneakers</h2>
+                                        <button type="button" className="btn btn-light">SHOP</button>
+                                    </div>
+                                </div>
                             </div>
-                            <div>
-                                <h3>4</h3>
+                            <div className="col-12 slic_col12">
+                                <div className="card slic_card">
+                                    <img src="../images/product2.png" width="100%" alt=""/>
+                                    <div className="write">
+                                        <h1>RS-Fast INTL </h1>
+                                        <h2>men's Sneakers</h2>
+                                        <button type="button" className="btn btn-light">SHOP</button>
+                                    </div>
+                                </div>
                             </div>
-                            <div>
-                                <h3>5</h3>
+                            <div className="col-12 slic_col12">
+                                <div className="card slic_card">
+                                    <img src="../images/prodcut.png" width="100%" alt=""/>
+                                    <div className="write">
+                                        <h1>Clyde Core Foil</h1>
+                                        <h2>Women's Sneakers</h2>
+                                        <button type="button" className="btn btn-light">SHOP</button>
+                                    </div>
+                                </div>
                             </div>
-                            <div>
-                                <h3>6</h3>
+                            <div className="col-12 slic_col12">
+                                <div className="card slic_card">
+                                    <img src="../images/product2.png" width="100%" alt=""/>
+                                    <div className="write">
+                                        <h1>RS-Fast INTL </h1>
+                                        <h2>men's Sneakers</h2>
+                                        <button type="button" className="btn btn-light">SHOP</button>
+                                    </div>
+                                </div>
                             </div>
                         </Slider>
                     </div>
